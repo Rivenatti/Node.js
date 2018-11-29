38f7222c8124a52fda65c6c07b07e54e2007f00f
Marcin <Rivenatti@gmail.com>
2018-11-29 17:30:01 +0100

    my notes

--- express_jwt_authentication/routes/profile.js
+++ express_jwt_authentication/routes/profile.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const userProfile = express.Router();
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
+const checkAuth = require("../middleware/check-auth");
 
-userProfile.get(
-  "/profile",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    return res.json({
-      id: req.user.id,
-      name: req.user.name,
-      organization: req.user.organization,
-      email: req.body.email
-    });
-  }
-);
+userProfile.get("/profile", checkAuth, (req, res) => {
+  return res.json({
+    id: req.user.id,
+    name: req.user.name,
+    organization: req.user.organization,
+    email: req.body.email
+  });
+});
 
 module.exports = userProfile;
